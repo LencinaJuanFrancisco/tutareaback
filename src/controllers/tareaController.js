545b50248df0b16ead7ac563eac754cbd2046e61
LencinaJuanFrancisco <juanfranciscolencina@gmail.com>
2023-02-20 10:53:03 -0300

    mostrandole algo a fcacu

--- src/controllers/tareaController.js
+++ src/controllers/tareaController.js
@@ -1,7 +1,9 @@
 // import models de tarea
 const tarea = {
     listarTodos:(req,res)=>{},
-    buscar:(req,res)=>{},
+    buscar:(req,res)=>{
+        console.log("con facu")
+    },
     crear:(req,res)=>{},
     editar:(req,res)=>{},
     borrar:(req,res)=>{}
