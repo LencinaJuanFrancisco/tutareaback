32dfccaad72c4af6bbad1f3d2843f1e412475fd9
contrabass88 <118854985+contrabass88@users.noreply.github.com>
2023-02-28 19:22:09 -0300

    Tareas listas

--- src/controllers/tareaController.js
+++ src/controllers/tareaController.js
@@ -1,4 +1,6 @@
 import { Task } from '../Schemas/Task.js'
+import { validateIdParamas } from '../helpers/validateIdParamas.js'
+
 const tarea = {
     listarTodos: async (req, res) => {
         try {
@@ -10,8 +12,18 @@ const tarea = {
         }
 
     },
-    buscar: (req, res) => {
-        console.log("con facu")
+    listarUno: async (req, res) => {
+        try {
+            const { id } = req.params;
+            if (validateIdParamas(id)) {
+                const allTask = await Task.findById(id);
+            return allTask ? res.status(200).json({ message: "Listado", data: allTask })
+                    : res.status(200).json({ message: "Aun no hay registros de Tareas" })
+            }
+        }
+        catch (error) {
+            res.status(400).json({ message: error.message });
+        }
     },
     crear: async (req, res) => {
         const data = req.body
@@ -32,7 +44,38 @@ const tarea = {
         }
     },
 
-    editar: (req, res) => { },
-    borrar: (req, res) => { }
+    editar: async (req, res) => {
+        const { id } = req.params;
+        const body = req.body;
+        try {
+            if (validateIdParamas(id)) {
+                const upDateTask = await Task.findByIdAndUpdate(id, body, {
+                    new: true,
+                });
+
+                if (upDateTask != null) {
+                    return res.status(200).json({ message: "Editado", data: upDateTask })
+
+                }
+                return res.status(401).json({ message: "Usuario no encontrado" });
+            }
+            return res.status(400).json({ message: "Formato del ID no valido" });
+        } catch (error) {
+            return res.status(400).json({ message: error.message });
+        }
+    },
+    borrar: async (req, res) => {
+        const { id } = req.params
+        try {
+            if (validateIdParamas(id)) {
+                const rta = await Task.findByIdAndDelete(id)
+                if (rta) return res.status(200).json({ message: `Tarea con ID ${id} fue eliminado` })
+                return res.status(401).json({ message: "Tarea no encontrado" })
+            }
+            return res.status(400).json({ message: "Formato del ID no valido" })
+        } catch (error) {
+            return res.status(400).json({ message: error.message });
+        }
+    },
 }
 export default tarea
--- src/routers/tareaRouter.js
+++ src/routers/tareaRouter.js
@@ -4,7 +4,11 @@ import tareaController from './../controllers/tareaController.js'
 const router = Router()
 
 router.get("/",tareaController.listarTodos)
+router.get('/:id',tareaController.listarUno)
 router.post("/",tareaController.crear)
+router.patch('/:id',tareaController.editar)
+router.delete('/:id',tareaController.borrar)
+
 
 
 export default router
