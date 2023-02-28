e4fc5ef117bfde2cd02514b94e63e8ca03b050e3
LencinaJuanFrancisco <juanfranciscolencina@gmail.com>
2023-02-28 06:55:17 -0300

    configuraciones en .env

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,6 +1,5 @@
-
 import { generateAccessToken } from "../helpers/generateAccessToken.js";
-import { hashPassword,comparePassword } from "../helpers/hashPassword.js";
+import { hashPassword, comparePassword } from "../helpers/hashPassword.js";
 import { validateIdParamas } from "../helpers/validateIdParamas.js";
 
 import { User } from "./../Schemas/Users.js";
@@ -9,8 +8,9 @@ const usuario = {
   listarTodos: async (req, res) => {
     try {
       const allUsers = await User.find();
-      allUsers.length > 0 ?  res.status(200).json({ message: "Listado", data: allUsers })
-                          :  res.status(200).json({ message: "Aun no hay registros de Usuarios" })
+      allUsers.length > 0
+        ? res.status(200).json({ message: "Listado", data: allUsers })
+        : res.status(200).json({ message: "Aun no hay registros de Usuarios" });
     } catch (error) {
       res.status(400).json({ message: error.message });
     }
@@ -22,11 +22,11 @@ const usuario = {
     try {
       if (validateIdParamas(id)) {
         const rta = await User.findById(id);
-       return  rta
+        return rta
           ? res.status(200).json({ data: rta })
           : res.status(401).json({ message: "Registro no encontrado" });
       }
-     return res.status(400).json({ message: "Formato del ID no valido" });
+      return res.status(400).json({ message: "Formato del ID no valido" });
     } catch (error) {
       res.status(400).json({ message: error.message });
     }
@@ -39,12 +39,12 @@ const usuario = {
       const newUser = new User({ ...data, password: passHas });
       const saveUser = await newUser.save();
 
-      return  res.status(200).json({
+      return res.status(200).json({
         message: "usuario creado",
         data: saveUser,
       });
     } catch (error) {
-     return  res.status(400).json({ message: error.message });
+      return res.status(400).json({ message: error.message });
     }
   },
   editar: async (req, res) => {
@@ -57,61 +57,64 @@ const usuario = {
           new: true,
         });
 
-       if(upDateUser != null){
-         return res.status(200).json({ message: "Editado", data: upDateUser })
-
-       }
+        if (upDateUser != null) {
+          return res.status(200).json({ message: "Editado", data: upDateUser });
+        }
         return res.status(401).json({ message: "Usuario no encontrado" });
       }
       return res.status(400).json({ message: "Formato del ID no valido" });
     } catch (error) {
-      return  res.status(400).json({ message: error.message });
+      return res.status(400).json({ message: error.message });
     }
   },
-  borrar: async(req, res) => {
-    const {id} = req.params
+  borrar: async (req, res) => {
+    const { id } = req.params;
     try {
       if (validateIdParamas(id)) {
-        const rta = await User.findByIdAndDelete(id)
-      if(rta) return res.status(200).json({message:`Usuario con ID ${id} sue eliminado`})
-            return res.status(401).json({message: "Usuario no encontrado"})
+        const rta = await User.findByIdAndDelete(id);
+        if (rta)
+          return res
+            .status(200)
+            .json({ message: `Usuario con ID ${id} sue eliminado` });
+        return res.status(401).json({ message: "Usuario no encontrado" });
       }
-      return res.status(400).json({message:"Formato del ID no valido"})
+      return res.status(400).json({ message: "Formato del ID no valido" });
     } catch (error) {
       return res.status(400).json({ message: error.message });
     }
   },
-  login:async (req,res)=>{
-    
-    const {email, password} = req.body
-    console.log("log del body paramas", email , password);
+  login: async (req, res) => {
+    const { email, password } = req.body;
+    //console.log("log del body paramas", email , password);
     try {
-        const findUser = await User.find({email:email})
-        console.log('findUsers',findUser);
-        if (findUser.length > 0) {
-          //una vez encontrado el usuario , comparo las password
-          const rta = await comparePassword(password,findUser[0].password)
-          // rta es boolean
-          console.log("rta de compare" , rta);
-          if (rta === true) {
-              //si rta es true , genero el JWT
-              const jwtUser = await generateAccessToken(findUser[0].name)
-              console.log('log del token',jwtUser);
-              return res.header('autorizado',jwtUser).json({
-                message:"Usuario autenticado",
-                token: jwtUser
-              })
-          }else{
-            console.log("else de la respuesta de rta");
-          return  res.status(401).json({message:"LAS CREDENCIALES NO SON CORRECTAS"})
-
-          }
+      const findUser = await User.find({ email: email });
+      // console.log('findUsers',findUser);
+      if (findUser.length > 0) {
+        //una vez encontrado el usuario , comparo las password
+        const rta = await comparePassword(password, findUser[0].password);
+        // rta es boolean
+        // console.log("rta de compare" , rta);
+        if (rta === true) {
+          //si rta es true , genero el JWT
+          const jwtUser = await generateAccessToken(findUser[0].name);
+          // console.log('log del token',jwtUser);
+          return res.header("autorizado", jwtUser).json({
+            message: "Usuario autenticado",
+            token: jwtUser,
+          });
+        } else {
+       
+          //utilizamos el mismo msg para indicar al usuario el error pero sin dar pistas si es el usuario o el password el incorrecto
+          return res
+            .status(401)
+            .json({ message: "Usuario no encontrado" });
         }
-       return res.status(401).json({message:"Usuario no encontrado"})
+      }
+      return res.status(401).json({ message: "Usuario no encontrado" });
     } catch (error) {
-     console.log("log del chatch de login" , error);
+      console.log("log del chatch de login", error);
       res.status(400).json({ message: error.message });
     }
-  }
+  },
 };
 export default usuario;
--- src/helpers/validateIdParamas.js
+++ src/helpers/validateIdParamas.js
@@ -2,7 +2,7 @@ export function validateIdParamas(paramsId){
    // utiulizo expreciones regulares para definir el formato de ID de Mongo , para validar el ID enviado por parametro
    
    if ( !paramsId.match(/^[0-9a-fA-F]{24}$/)  ) {
-    console.log("longitud del paramsId",paramsId.length);
+    //console.log("longitud del paramsId",paramsId.length);
     return false
    }
    return true
