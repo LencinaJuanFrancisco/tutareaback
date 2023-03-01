e4ba7ea2c69a205a04e10c3d2b9e88d2a5d5305c
contrabass88 <118854985+contrabass88@users.noreply.github.com>
2023-03-01 18:29:35 -0300

    creación del controllers Proyectos, Schemas y routers

--- src/controllers/proyectoController.js
+++ src/controllers/proyectoController.js
@@ -1,9 +1,48 @@
+import { validateIdParamas } from '../helpers/validateIdParamas.js'
 import Proyect from '../Schemas/Proyect.js'
  const proyecto = {
-    listarTodos:(req,res)=>{},
-    listarUno:(req,res)=>{},
-    crear:(req,res)=>{},
-    editar:(req,res)=>{},
-    borrar:(req,res)=>{}
+    listarTodos:async(req,res)=>{
+        try {
+            const allProyect = await Proyect.find().populate("task")
+                                                   .populate('collaborator',"name email")
+
+            allProyect.length > 0 ? res.status(200).json({message:"Totos los proyectos",data: allProyect})
+                                  : res.status(401).json({message:"Ahun no hay proyectos"})   
+        } catch (error) {
+            return res.status(400).json(error.message)
+        }
+    },
+    listarUno:async(req,res)=>{
+        const {id}= req.params
+        try {
+            if (validateIdParamas(id)) {
+                const oneProyect = await Proyect.findById(id).populate("task")
+                                                             .populate('collaborator',"name email")
+
+                 return oneProyect ? res.status(200).json({ message: "Listado", data: oneProyect })
+                                : res.status(200).json({ message: "Proyecto no encontrado" })
+            }
+            return res.status(401).json({message:"formato de ID no valido"})
+        } catch (error) {
+            
+        }
+    },
+    crear:async(req,res)=>{
+        const newProyect =new Proyect(req.body)
+        newProyect.createUser = req.uid
+        try {
+            const saveProyect = await newProyect.save()
+            return res.status(200).json({message:"Proyecto Creado",data:saveProyect})
+        } catch (error) {
+            return res.status(401).json({message:"error al crear el proyect",error: error.message})
+        }
+    
+    },
+    editar:async(req,res)=>{},
+    borrar:async(req,res)=>{},
+    agregarTarea:async(req,res)=>{},
+    agregarColaborador:async(req,res)=>{},
+    eliminarColaborador:async(req,res)=>{},
+
 }
  export default proyecto
--- src/routers/proyectoRouter.js
+++ src/routers/proyectoRouter.js
@@ -1,10 +1,10 @@
 import {Router} from 'express'
 import proyectoControlles from './../controllers/proyectoController.js'
-
+import {validateToken} from '../middlewares/validateToken.js'
 const router = Router()
 
 router.get("/",proyectoControlles.listarTodos)
-router.post("/",proyectoControlles.crear)
+router.post("/",validateToken,proyectoControlles.crear)
 router.get('/:id',proyectoControlles.listarUno)
 router.patch('/:id',proyectoControlles.editar)
 router.delete('/:id',proyectoControlles.borrar)
