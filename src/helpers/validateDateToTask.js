797ed80304d07875ca12fe7ac20c80ad76ab5db6
LencinaJuanFrancisco <juanfranciscolencina@gmail.com>
2023-03-11 11:12:27 -0300

    ..

---
+++ src/helpers/validateDateToTask.js
@@ -0,0 +1,12 @@
+
+
+export const validateDateToTask=(taskDateEnd,proyectDateEnd)=>{
+    
+
+    const now = new Date();
+     
+     if (taskDateEnd > proyectDateEnd || taskDateEnd < now ) {
+        return true
+     }
+    
+}
